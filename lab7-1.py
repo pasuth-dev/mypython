47fb7dfa865261e9f39913282ed6b04a6e188a96
USER-STU <pasuth.kp@gmail.com>
2024-07-19 16:14:11 +0700

    update

---
+++ lab7-1.py
@@ -0,0 +1,14 @@
+import Tkinter as tk #python2
+
+def showme():
+app = tk.TK()
+app.geometry("500*300")
+app.titel('เขียนโดย พูมี้่')
+L1 = tk.Label(text="welcome to phython")
+L2 = tk.Label(text="by poomie ")
+L3 = tk.Label(text="text of sell item")
+E1 = tk.Entery
+L1.place(x=50, y=10)
+L2.place(x=50 ,y=50)
+L3.place(x=50 ,y=50)
+E1.place(x=50 ,y=50)
